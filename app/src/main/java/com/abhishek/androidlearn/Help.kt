e1d9e174a56e907b837438def0362d92028fba75
abhishek44gl <abhishek44priv@gmail.com>
2023-01-16 11:52:11 +0530

    initial

--- app/src/main/java/com/abhishek/androidlearn/Help.kt
+++ app/src/main/java/com/abhishek/androidlearn/Help.kt
@@ -1,11 +1,41 @@
 package com.abhishek.androidlearn
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Help {
-    companion object{
-        fun log_(msg: Any?) {
-            Log.d("---------------",msg.toString())
+    companion object {
+        fun log_(msg: Any?, e: Throwable? = null) {
+            Log.d("dell", msg.toString(), e)
+        }
+
+        //method to convert your text to image
+        fun textAsBitmap(
+            text: String = "png",
+            textSize: Float = 20f,
+            textColor: Int = Color.BLACK
+        ): Bitmap? {
+            val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+            paint.textSize = textSize
+            paint.color = textColor
+            paint.textAlign = Paint.Align.LEFT
+            val baseline: Float = -paint.ascent() // ascent() is negative
+            val width = (paint.measureText(text) + 0.0f).toInt() // round
+            val height = (baseline + paint.descent() + 0.0f).toInt()
+            val image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(image)
+            canvas.drawText(text, 0f, baseline, paint)
+            return image
         }
     }
+}
+
+fun AppCompatActivity.showMessage(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/com/abhishek/androidlearn/MainActivity.kt
+++ app/src/main/java/com/abhishek/androidlearn/MainActivity.kt
@@ -1,16 +1,113 @@
 package com.abhishek.androidlearn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
 import com.abhishek.androidlearn.databinding.ActivityMainBinding
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-   private  lateinit var binding:ActivityMainBinding
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       binding=ActivityMainBinding.inflate(layoutInflater)
-       setContentView(binding.root)
-       //-------------------------------------
+    private val REQUEST_CODE_SAVE_PNG = 100
+    private lateinit var binding: ActivityMainBinding
+    private var color = MutableLiveData(Color.WHITE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            colorView.showText(false)
+            tvColorChooser.setOnClickListener {
+                val dialog = ColorDialogFragment()
+                color.value?.let { it1 -> dialog.setColor(it1) }
+                dialog.setOnClickListener { isOk, rgb ->
+                    if (isOk) {
+                        color.value = rgb
+                    }
+                }
+                dialog.show(supportFragmentManager, null)
+            }
+            //set png image to fab
+            floatingActionButton.setImageBitmap(Help.textAsBitmap())
+            floatingActionButton.setOnClickListener {
+                savePng()
+            }
+        }
+        color.observe(this) {
+            binding.colorView.setColor(it)
+        }
+    }
 
-   }
+    private fun savePng() {
+        if (checkPermission()) {
+            val filename = if (color.value != null) {
+                "${Color.red((color.value!!))}_${Color.green((color.value!!))}_${Color.blue((color.value!!))}"
+            } else
+                color.toString()
+            val dir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS + "/$filename.png")
+            dir.createNewFile()
+            try {
+                val out = FileOutputStream(dir)
+                val bitmap = binding.colorView.getBitmap()
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+                showMessage("file save at ${dir.absolutePath}")
+            } catch (e: IOException) {
+                Help.log_("can't save file", e)
+                showMessage("some error while saving file")
+            }
+            Help.log_(dir.absolutePath)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQUEST_CODE_SAVE_PNG
+            );
+        } else {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_SAVE_PNG -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    //granted
+                    savePng()
+                } else {
+                    //explain the user
+                }
+                return
+            }
+            else -> {
+
+            }
+        }
+    }
+
+
 }
+
