357dd2ee4081861e7456931dce4a3f5f9111597e
abhishek44gl <abhishek33gl@gmail.com>
2023-01-18 18:22:45 +0530

    update TimerManger

--- app/src/main/java/com/abhishek/androidlearn/MainActivity.kt
+++ app/src/main/java/com/abhishek/androidlearn/MainActivity.kt
@@ -7,24 +7,26 @@ import com.abhishek.androidlearn.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var count = 0
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       binding = ActivityMainBinding.inflate(layoutInflater)
-       setContentView(binding.root)
-       //-------------------------------------
-       val timerManager = TimerManager(500)
-       val timer1 = Timer(50)
-       binding.apply {
-           msg.setOnClickListener {
-               if (timer1.isResume) timer1.pause() else timer1.resume()
-           }
-           timer1.setOnTickListener {
-               msg.text = "$it"
-           }
-           root.setOnClickListener {
-               timer1.reset()
-           }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //-------------------------------------
+        val timerManager = TimerManager(50)
+        val timer1 = timerManager.createTimer(-1).apply {
+            start()
+        }
+        binding.apply {
+            msg.setOnClickListener {
+                if (timer1.isResume()) timer1.pause() else timer1.resume()
+            }
+            timer1.setOnTickListener {
+                msg.text = "$it"
+            }
+            root.setOnClickListener {
+                timer1.reset()
+            }
        }
-       timerManager.addTimer(timer1)
    }
+
 }
--- app/src/main/java/com/abhishek/androidlearn/TimerManger.kt
+++ app/src/main/java/com/abhishek/androidlearn/TimerManger.kt
@@ -3,55 +3,76 @@ package com.abhishek.androidlearn
 import android.os.Handler
 import android.os.Looper
 
-class Timer(var total: Int = 10) {
-    var isResume = true
-    var isStarted = true
-    var count = 0
-    private var tickCallback: ((count: Int) -> Unit)? = null
-    fun start() {
-        isStarted = true
-        isResume = true
-    }
-
-    fun increment(value: Int) {
-        total += value
-    }
-
-    fun resetAndStop() {
-        count = 0
-        isStarted = false
-        isResume = false
-        onTick(0)
-    }
-
-    fun reset() {
-        count = 0
-    }
-
-    fun pause() {
-        isResume = false
-    }
-
-    fun resume() {
-        isResume = true
-    }
-
-    fun setOnTickListener(f: (count: Int) -> Unit) {
-        tickCallback = f
-    }
-
-    fun onTick(count: Int) {
-        tickCallback?.invoke(count)
-    }
+interface Timer {
+    fun start()
+    fun increment(value: Int)
+    fun resetAndStop()
+    fun reset()
+    fun pause()
+    fun resume()
+    fun isResume(): Boolean
+    fun isStarted(): Boolean
+    fun setOnTickListener(f: (count: Int) -> Unit)
 }
 
 class TimerManager(val delay: Int = 1000) {
+    private inner class TmTimer(var total: Int = 10) : Timer {
+        var mIsResume = false
+        var mIsStarted = false
+        var count = 0
+        private var tickCallback: ((count: Int) -> Unit)? = null
+        override fun start() {
+            mIsStarted = true
+            mIsResume = true
+        }
+
+        override fun increment(value: Int) {
+            total += value
+        }
+
+        override fun resetAndStop() {
+            count = 0
+            mIsStarted = false
+            mIsResume = false
+            onTick(0)
+        }
+
+        override fun reset() {
+            count = 0
+        }
+
+        override fun pause() {
+            mIsResume = false
+        }
+
+        override fun resume() {
+            mIsResume = true
+        }
+
+        override fun isResume(): Boolean = mIsResume
+        override fun isStarted(): Boolean = isStarted
+
+        override fun setOnTickListener(f: (count: Int) -> Unit) {
+            tickCallback = f
+        }
+
+        fun onTick(count: Int) {
+            tickCallback?.invoke(count)
+        }
+    }
+
     private val handler = Handler(Looper.getMainLooper())
     private var isStarted = false
-    private val timers = arrayListOf<Timer>()
-    fun addTimer(timer: Timer) {
+    private val timers = arrayListOf<TmTimer>()
+    private fun addTimer(timer: TmTimer) {
         timers.add(timer)
         start()
+    }
+
+    fun createTimer(total: Int = 10): Timer {
+        val timer = TmTimer(total)
+        addTimer(timer)
+        return timer
     }
 
     fun removeTimer(timer: Timer) {
@@ -74,7 +95,7 @@ class TimerManager(val delay: Int = 1000) {
 
     private fun updateAllTimer() {
         timers.forEach {
-            if (it.isStarted && it.isResume) {
+            if (it.mIsStarted && it.mIsResume) {
                 if (it.total == -1) {
                     it.onTick(it.count)
                     it.count++
